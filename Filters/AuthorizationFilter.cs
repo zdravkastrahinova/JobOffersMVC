96750946da1fbb4ea93bf28cb2d140a51d412ad3
Zdravka Strahinova <zdravkastrahinova1022@gmail.com>
2020-10-24 12:06:50 +0300

    Add filters to controllers' actions

---
+++ Filters/AuthorizationFilter.cs
@@ -0,0 +1,18 @@
+﻿using JobOffersMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace JobOffersMVC.Filters
+{
+    public class AuthorizationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (AuthenticationService.LoggedUser != null)
+            {
+                context.HttpContext.Response.Redirect("/Users/List");
+                context.Result = new EmptyResult();
+            }
+        }
+    }
+}
