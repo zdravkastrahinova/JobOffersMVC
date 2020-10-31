adee4b1e4b31d3069560e1c8b72aa8a27275e048
Zdravka Strahinova <zdravkastrahinova1022@gmail.com>
2020-10-31 12:20:14 +0200

    Update repositories. Add abstractions

---
+++ Repositories/Abstractions/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JobOffersMVC.Repositories.Abstractions
+{
+    public interface IBaseRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Insert(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
---
+++ Repositories/Implementations/UsersRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using JobOffersMVC.Models;
+using JobOffersMVC.Repositories.Abstractions;
+
+namespace JobOffersMVC.Repositories
+{
+    public class UsersRepository : BaseRepository<User>, IUsersRepository
+    {
+        public UsersRepository(JobOffersDbContext context)
+            : base (context)
+        {
+            
+        }
+
+        public User GetByUsernameAndPassword(string username, string password)
+        {
+            return GetAll().FirstOrDefault(user => user.Username == username && user.Password == password);
+        }
+    }
+}
