ccae5e927a00b57861c4cefdcc61e2b8c02c1f42
Zdravka Strahinova <zdravkastrahinova1022@gmail.com>
2020-10-31 12:17:49 +0200

    Update models and build relations

--- Models/JobOffer.cs
+++ Models/JobOffer.cs
@@ -1,11 +1,17 @@
-﻿namespace JobOffersMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobOffersMVC.Models
 {
     public class JobOffer : BaseModel
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(500)]
         public string Description { get; set; }
 
         public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
