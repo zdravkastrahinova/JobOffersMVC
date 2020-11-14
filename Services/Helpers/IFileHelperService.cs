b627adf57435172ae21b43ede52678000de76d83
Zdravka Strahinova <zdravka.strahinova@primeholding.com>
2020-11-14 12:37:18 +0200

    Create file helper service for image upload

---
+++ Services/Helpers/IFileHelperService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace JobOffersMVC.Services.Helpers
+{
+    public interface IFileHelperService
+    {
+        string BuildFilePath(string directory, string filePath);
+
+        void CreateFile(IFormFile imageFile, string filePath);
+    }
+}
