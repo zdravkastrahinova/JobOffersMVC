2b85629317b17376fdc8202188ef07657e884547
Zdravka Strahinova <zdravka.strahinova@primeholding.com>
2020-11-14 12:36:42 +0200

    Configure application for image upload

---
+++ Migrations/20201114092526_AddImagePathToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobOffersMVC.Migrations
+{
+    public partial class AddImagePathToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Users");
+        }
+    }
+}
